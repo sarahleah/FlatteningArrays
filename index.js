0b76be13af088729e3bfff1db118edf9cc72a6fa
sarahleah <>
2021-05-28 06:18:48 +0000

    Finalized using all criteria

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let arrays = [[1, 2, 3], [4, 5], [6]];
 const flatten = array => {
-  let newArray = [];
-  return newArray.concat.apply([], arrays);
+  const reducer = (accumulator, currentValue) => {
+    return accumulator.concat(currentValue);
+  }
+ return arrays = arrays.reduce(reducer, []);
 }
 console.log(flatten(arrays));
 // → [1, 2, 3, 4, 5, 6]
